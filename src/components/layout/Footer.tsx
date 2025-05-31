2b260c3080e30dd75868b7ab24658daf842d08ff
S-Del <s-del.pg@protonmail.com>
2025-05-31 09:48:57 +0900

    [change] トップページの配色

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Footer: React.FC = () => {
   return (
-    <footer className="bg-stone-500 text-gray-200 p-3 mt-auto text-center text-xs">
+    <footer className="bg-[rgb(130,110,100)] text-gray-100 p-3 mt-auto text-center text-xs">
       {/* mt-auto は親がflex flex-col min-h-screenの場合にフッターを下部に押しやる */}
       <a href="https://gitlab.com/S-Del" target="_blank" rel="noopener noreferrer">
         <p>&copy; {new Date().getFullYear()} S-Del</p>
