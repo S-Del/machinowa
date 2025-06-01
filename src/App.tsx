c5abb9c482b9bfb8de0a928ff3610599c30adee1
S-Del <s-del.pg@protonmail.com>
2025-06-01 12:43:09 +0900

    [add] イベント詳細ページ

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { Route, Routes } from "react-router-dom";
+
 import RegionSelectorTopPage from "./pages/RegionSelectorTopPage";
 import LocalEventListPage from "./pages/LocalEventListPage";
+import EventDetailPage from "./pages/EventDatailsPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<RegionSelectorTopPage />} />
+
       <Route path="/events/:regionId" element={<LocalEventListPage />} />
+      <Route path="/events/:regionId/opportunities" element={<div>お手伝い募集一覧ページ (仮)</div>} />
+      <Route path="/events/:regionId/new" element={<div>イベント開催ページ (仮)</div>} />
+      <Route path="/events/filter" element={<div>イベント絞り込みページ (仮)</div>} />
+
+      <Route path="/event/:eventId" element={<EventDetailPage />} />
     </Routes>
   );
 };
