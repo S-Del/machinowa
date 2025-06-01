63c9bfc1083dd8e839bdd6b982dfabb5c85bc7f7
S-Del <s-del.pg@protonmail.com>
2025-06-01 22:18:24 +0900

    [fix] 注目イベントをフィルタリングするように

--- src/pages/LocalEventListPage.tsx
+++ src/pages/LocalEventListPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { Header } from "../components/layout/Header";
@@ -39,6 +39,26 @@ const LocalEventListPage: React.FC = () => {
 
   // if (error) {...}
 
+  /* events を元に featuredEvents を計算 (取得)。
+   * featuredEvents をメモ化しておくことで、再レンダーが生じた際に再計算 (再取得) しなくて済む。
+   * 再レンダーは以下のタイミングで発生する。
+   * 1. state が更新された時
+   * 2. props が更新された時
+   * 3. 親コンポーネント (このコンポーネントから見ると App.tsx) が更新された時。
+   *
+   * このページは、
+   * - state はページが読み込まれたときに fetch する events のみ。（途中で更新されない）
+   * - props は受け取らない。
+   * - 親コンポーネント (App.tsx) も status や props を持たない。
+   * また、現状クエリパラメータを必要とするページが無いため、
+   * - クエリパラメータ由来の BrowserRouter の location オブジェクト変更が起こらない。
+   * という状況のため、この useMemo によるメモ化が活用されるケースは少ないかほぼ起こらないが、
+   * 将来的な変更への対応と、自身の学習のために使用している。
+   */
+  const featuredEvents = useMemo(() => {
+    return events.filter(event => event.isFeatured)
+  }, [events])
+
   if (events.length === 0) {
     // events が空であった場合の処理。
     // エラーでは無く、純粋に「この地域に開催予定のイベントが無い」状態なので、
@@ -50,7 +70,7 @@ const LocalEventListPage: React.FC = () => {
       <Header />
 
       <main className="grow">
-        <FeaturedEventsCarousel events={events} />
+        <FeaturedEventsCarousel events={featuredEvents} />
         <EventCardsSection title="イベント一覧" events={events} />
 
         <div className="p-4 grid grid-cols-1 lg:grid-cols-2 gap-4">
